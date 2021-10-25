88a2b720ee3e2e2c07d7c2488869aae18578e275
Andrei Babeanu <babeanu.andrei99@gmail.com>
2021-10-25 13:25:48 +0300

    Deployed the backend with additional changes

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,20 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_BY_CREATOR, FETCH_POST, CREATE, UPDATE, DELETE, LIKE, COMMENT } from '../constants/actionTypes';
+import {
+  FETCH_ALL,
+  FETCH_BY_SEARCH,
+  FETCH_BY_CREATOR,
+  FETCH_POST,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+  COMMENT,
+} from "../constants/actionTypes";
 
 export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
-    case 'START_LOADING':
+    case "START_LOADING":
       return { ...state, isLoading: true };
-    case 'END_LOADING':
+    case "END_LOADING":
       return { ...state, isLoading: false };
     case FETCH_ALL:
       return {
@@ -19,12 +29,17 @@ export default (state = { isLoading: true, posts: [] }, action) => {
     case FETCH_POST:
       return { ...state, post: action.payload.post };
     case LIKE:
-      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        ),
+      };
     case COMMENT:
       return {
         ...state,
         posts: state.posts.map((post) => {
-          if (post._id == +action.payload._id) {
+          if (post._id === action.payload._id) {
             return action.payload;
           }
           return post;
@@ -33,11 +48,18 @@ export default (state = { isLoading: true, posts: [] }, action) => {
     case CREATE:
       return { ...state, posts: [...state.posts, action.payload] };
     case UPDATE:
-      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        ),
+      };
     case DELETE:
-      return { ...state, posts: state.posts.filter((post) => post._id !== action.payload) };
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== action.payload),
+      };
     default:
       return state;
   }
 };
-
--- server/index.js
+++ server/index.js
@@ -1,26 +1,34 @@
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-import postRoutes from './routes/posts.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
 import userRouter from "./routes/user.js";
 
 const app = express();
+dotenv.config();
 
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.get("/", (req, res) => {
+  res.send("hello there");
+});
+
+app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://crixus:crixus123@cluster0.gzs7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set("useFindAndModify", false);
